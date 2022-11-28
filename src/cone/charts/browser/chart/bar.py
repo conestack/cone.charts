f0e1e1d4f20c07d56e20354ed0869957aaea8534
toalba <tb@kleinundpartner.at>
2022-11-28 16:01:18 +0100

    chart types cleanup

--- src/cone/charts/browser/chart/bar.py
+++ src/cone/charts/browser/chart/bar.py
@@ -6,6 +6,7 @@ class BarChartTile(ChartTile):
     barchart options documentation:
     https://www.chartjs.org/docs/latest/charts/bar.html
     """
+    
     chart_factory = 'cone_charts.BarChartTile'
 
     chart_options ={
@@ -20,16 +21,4 @@ class BarChartTile(ChartTile):
 
     @staticmethod
     def chart_data(model, request):
-        """
-        {
-            'labels': [str(i) for i in range(len(model.data))],
-            'datasets': [{
-                'label': 'My First Dataset',
-                'data': model.data,
-                'fill': False,
-                'borderColor': 'rgb(75, 192, 192)',
-                'tension': 0.1
-            }]
-        }
-        """
-        raise NotImplementedError('')
+        raise NotImplementedError('Chart data not implemented')
--- src/cone/charts/browser/chart/polar.py
+++ src/cone/charts/browser/chart/polar.py
@@ -2,21 +2,13 @@ from .chart import ChartTile
 
 
 class PolarChartTile(ChartTile):
+    """
+    polarchart options documentation:
+    https://www.chartjs.org/docs/latest/charts/polar.html
+    """
+    
     chart_factory = 'cone_charts.PolarChartTile'
-    # polarchart options documentation:
-    # https://www.chartjs.org/docs/latest/charts/polar.html
 
     @staticmethod
     def chart_data(model, request):
-        if not model.data:
-            raise ValueError('No data available')
-        return {
-            'labels': [str(i) for i in range(len(model.data))],
-            'datasets': [{
-                'label': 'My First Dataset',
-                'data': model.data,
-                'fill': False,
-                'borderColor': 'rgb(75, 192, 192)',
-                'tension': 0.1
-            }]
-        }
+        raise NotImplementedError('Chart data not implemented')
