7c88a1278c2364666c2e628f94c49077ae7dc56b
toalba <tb@kleinundpartner.at>
2022-12-07 14:42:15 +0100

    add imports to tests

--- src/cone/charts/tests/test_browserchart.py
+++ src/cone/charts/tests/test_browserchart.py
@@ -5,6 +5,10 @@ from cone.charts.browser.chart import chart_tile
 from cone.charts.browser.line import LineChartTile
 from cone.charts.browser.pie import PieChartTile
 from cone.charts.browser.polar import PolarChartTile
+from cone.charts.browser.radar import RadarChartTile
+from cone.charts.browser.scatter import ScatterChartTile
+from cone.charts.browser.doughnut import DoughnutChartTile
+from cone.charts.browser.bubble import BubbleChartTile
 from node.tests import NodeTestCase
 
 
@@ -86,6 +90,75 @@ class TestBrowserCharts(NodeTestCase):
         chart_tile.render()
         with self.assertRaises(NotImplementedError):
             chart_tile.chart_data(chart_tile.model, chart_tile.request)
+
+    def test_radar_chart(self):
+        """Testing the radar chart tile and the pre-configurations"""
+        request = self.layer.new_request()
+        chart_tile = RadarChartTile()
+        chart_tile.request = request
+        chart_tile.model = BaseNode()
+
+        self.assertEqual(chart_tile.chart_id, 'cone-chart')
+        self.assertEqual(chart_tile.chart_css, 'cone-chart')
+        self.assertEqual(chart_tile.chart_factory, 'cone_charts.ChartTile')
+        self.assertEqual(chart_tile.chart_type, 'radar')
+        self.assertEqual(chart_tile.chart_options, None)
+        self.assertEqual(chart_tile.chart_params, {})
+        chart_tile.render()
+        with self.assertRaises(NotImplementedError):
+            chart_tile.chart_data(chart_tile.model, chart_tile.request)
+
+    def test_scatter_chart(self):
+        """Testing the scatter chart tile and the pre-configurations"""
+        request = self.layer.new_request()
+        chart_tile = ScatterChartTile()
+        chart_tile.request = request
+        chart_tile.model = BaseNode()
+
+        self.assertEqual(chart_tile.chart_id, 'cone-chart')
+        self.assertEqual(chart_tile.chart_css, 'cone-chart')
+        self.assertEqual(chart_tile.chart_factory, 'cone_charts.ChartTile')
+        self.assertEqual(chart_tile.chart_type, 'scatter')
+        self.assertEqual(chart_tile.chart_options, None)
+        self.assertEqual(chart_tile.chart_params, {})
+        chart_tile.render()
+        with self.assertRaises(NotImplementedError):
+            chart_tile.chart_data(chart_tile.model, chart_tile.request)
+
+    def test_bubble_chart(self):
+        """Testing the bubble chart tile and the pre-configurations"""
+        request = self.layer.new_request()
+        chart_tile = BubbleChartTile()
+        chart_tile.request = request
+        chart_tile.model = BaseNode()
+
+        self.assertEqual(chart_tile.chart_id, 'cone-chart')
+        self.assertEqual(chart_tile.chart_css, 'cone-chart')
+        self.assertEqual(chart_tile.chart_factory, 'cone_charts.ChartTile')
+        self.assertEqual(chart_tile.chart_type, 'bubble')
+        self.assertEqual(chart_tile.chart_options, None)
+        self.assertEqual(chart_tile.chart_params, {})
+        chart_tile.render()
+        with self.assertRaises(NotImplementedError):
+            chart_tile.chart_data(chart_tile.model, chart_tile.request)
+    
+    def test_doughnut_chart(self):
+        """Testing the doughnut chart tile and the pre-configurations"""
+        request = self.layer.new_request()
+        chart_tile = DoughnutChartTile()
+        chart_tile.request = request
+        chart_tile.model = BaseNode()
+
+        self.assertEqual(chart_tile.chart_id, 'cone-chart')
+        self.assertEqual(chart_tile.chart_css, 'cone-chart')
+        self.assertEqual(chart_tile.chart_factory, 'cone_charts.ChartTile')
+        self.assertEqual(chart_tile.chart_type, 'doughnut')
+        self.assertEqual(chart_tile.chart_options, None)
+        self.assertEqual(chart_tile.chart_params, {})
+        chart_tile.render()
+        with self.assertRaises(NotImplementedError):
+            chart_tile.chart_data(chart_tile.model, chart_tile.request)
+
 
     def test_chart_tile_decorator(self):
         """ Testing the chart tile decorator
